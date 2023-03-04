ae6049cf141d278542054e31e3716914864626c1
nevoniuk <nevoniuk@purdue.edu>
2023-03-04 10:10:51 -0500

    changed color

--- src/routes/UpdateOffer/+page.svelte
+++ src/routes/UpdateOffer/+page.svelte
@@ -113,14 +113,16 @@
 	}
 	
 	section {
-		border-right: 1px solid white;
-		padding: 1rem;
+		background-color: rgb(188, 181, 223);
+		border-radius: 4px;
+        box-shadow: 1px 1px 3px 0px rgba(34, 17, 66,0.4);
+		margin-right: 15px;
 		min-width: 20vw;
 		height: 30vh;
 		scroll-snap-align: start;
 		text-align: center;
 		position: relative;
-		background-color: rgba(224, 189, 240, 1);
+		
 	}
 	.offeritem {
 		color:black;
