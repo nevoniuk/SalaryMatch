077e8c1c016bc8e26ef0e6223cca2a5371e6a126
Lenny Meng <51030106+lmeng1214@users.noreply.github.com>
2023-03-30 12:07:26 -0400

    SAL-105: Added units and updated CSS to tailwind

--- src/routes/UpdateOffer/+page.svelte
+++ src/routes/UpdateOffer/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Heading, P, A, Label, Input } from 'flowbite-svelte'
+	import { Heading, Card, Button } from 'flowbite-svelte'
 	/** @type {import('./$types').PageData} */
 	import {authToken} from '../../auth'
 	console.log($authToken);
@@ -23,78 +23,34 @@
 	<meta name="description" content="Form to Update Job Offers" />
 </svelte:head>
 
-<main>
-	<div class = 'title'>
-		<Heading tag="h2" customSize="text-3xl font-bold ">Update Your Job Offers</Heading>	
+<div class = 'title'>
+	<Heading tag="h2" customSize="text-3xl font-bold ">Update Your Job Offers</Heading>	
+</div>
+<div class = 'subtitle'>
+	<Heading tag="h6" customSize="text-lg font-normal ">Select An Offer to Update</Heading>	
+</div>
+<div class= "slidercontainer">
+	<div class="slider">
+		{#await loading() then data}
+			{#each data as offer}
+				<Card class="flex-col justify-end bg-purple-300 mx-5 w-[500px] mb-0">
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Title: {offer.title}</h5>
+					<div class="mb-5 h-fit">
+						<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">Company: {offer.company}</p>
+						<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">Location: {offer.city_id}, {offer.state_id}</p>
+						<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">Base Salary: ${offer.salary}</p>
+						<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">Bonuses: ${offer.signing_bonus}</p>
+						<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">Relocation: ${offer.relocation_bonus}</p>
+					</div>
+
+					<Button href="/UpdateOffer/{offer.id}" class="w-fit">Edit This Offer<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" /></svg></Button>
+				</Card>
+			{/each}
+		{/await}
 	</div>
-	<div class = 'subtitle'>
-		<Heading tag="h6" customSize="text-lg font-normal ">Select An Offer to Update</Heading>	
-	</div>
-	<div  class= "slidercontainer">
-		<div class="slider">
-			{#await loading() then data}
-				{#each data as offer}
-					<section>
-						<div class= "titleAndEdit">
-							<div class="offeritem">
-								<p>
-									Official Job Title: {offer.title}
-								</p>
-							</div>
-							<div class="editbutton">
-								<span class="material-symbols-outlined orange-hover">
-									<a href="/UpdateOffer/{offer.id}">edit_square
-									</a>
-								</span>
-							</div>
-						</div>
-						<div class="offeritem">
-							<p>
-								Company: {offer.company}
-							</p>
-						</div>
-						<div class="offeritem">
-							<p>
-								Location: {offer.city_id}, {offer.state_id}
-							</p>
-						</div>
-						<div class="offeritem">
-							<p>
-								Salary: {offer.salary}
-							</p>
-						</div>
-						<div class="offeritem">
-							<p>
-								Sign-on bonus: {offer.signing_bonus}
-							</p>
-						</div>
-						<div class="offeritem">
-							<p>
-								Relocation Bonus: {offer.relocation_bonus}
-							</p>
-						</div>
-					</section>
-				{/each}
-			{/await}
-		</div>
-	</div>
-	
-</main>
+</div>
 
 <style>
-	.material-symbols-outlined {
-        font-size: 20px;
-    }
-	.title {
-			position: absolute;
-			top: 100px;
-			left:10px;
-		}
-	.subtitle {
-		position: absolute;
-		top: 250px;
-		left:10px;
-	}
 	.slidercontainer {
 		top:300px;
 		position: absolute;
@@ -106,31 +62,6 @@
 		display: flex;
 		-webkit-overflow-scrolling: touch;
 		overflow-x: scroll;
-		
-	}
-	
-	section {
-		background-color: rgb(188, 181, 223);
-		border-radius: 4px;
-        box-shadow: 1px 1px 3px 0px rgba(34, 17, 66,0.4);
-		margin-right: 15px;
-		min-width: 20vw;
-		height: 30vh;
-		scroll-snap-align: start;
-		text-align: center;
-		position: relative;
-		
-	}
-	.offeritem {
-		color:black;
-	}
-	.orange-hover:hover {
-        border-radius: 10px;
-        background-color: #FFC187;
-    }
-	.titleAndEdit {
-		display:flex;
-		flex-direction: row;
-		justify-content: space-between;
+		height: fit-content;
 	}
 </style>
